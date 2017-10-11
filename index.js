112631652e4bd29d3927f86eed7638688233ae66
Prabu Sathan <prabu.sathan@gmail.com>
2017-10-11 15:39:19 +0100

    Initialised react native app - eslint

---
+++ index.js
@@ -0,0 +1,14 @@
+// Import a Library to help create a Component
+
+import React from 'react';
+import ReactNative from 'react-native';
+
+// Create a Component - an object that produces something that can be placed on the screen
+
+const App = () => {
+  return (
+    <Text>Some Text</Text>
+  );
+};
+
+// Render it to the device
