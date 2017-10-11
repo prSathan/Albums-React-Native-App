1c4342bd150da5300739ade1fb2f297d4239d27b
Prabu Sathan <prabu.sathan@gmail.com>
2017-10-11 17:43:42 +0100

    View Tag with further styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,24 @@
 // Import Libraries for making a Component
 
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 // Make a Component
 
 const Header = () => {
-  const { textStyle } = styles;
+  const { textStyle, viewStyle } = styles;
 
-  return <Text style={textStyle}>Albums!</Text>;
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums!</Text>
+    </View>
+  );
 };
 
 const styles = {
+  viewStyle: {
+    backgroundColor: '#ef56b9'
+  },
   textStyle: {
     fontSize: 50
   }
