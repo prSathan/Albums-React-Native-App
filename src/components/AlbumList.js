0fbb8e8de0b98d525fca0454d01542e23f7a8ee6
Prabu Sathan <prabu.sathan@gmail.com>
2017-10-12 14:06:06 +0100

    Axios get request

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+  // cWM called the instance this component is about to be placed on the screen
   componentWillMount() {
-    console.log('componentWillMount in console');
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    .then(response => console.log(response));
   }
 
   render() {
