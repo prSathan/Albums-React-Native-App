dd52163f01db2eee064c299fb96d2a358340d2ed
Prabu Sathan <prabu.sathan@gmail.com>
2017-10-12 14:32:10 +0100

    Set initial state to empty array, setState method to re-render component  after data is retrieved

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,13 +3,15 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
+  state = { albums: [] };
   // cWM called the instance this component is about to be placed on the screen
   componentWillMount() {
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-    .then(response => console.log(response));
+    .then(response => this.setState({ albums: response.data }));
   }
 
   render() {
+    console.log(this.state);
     return (
       <View>
         <Text>Album List</Text>
@@ -19,3 +21,6 @@ class AlbumList extends Component {
 }
 
 export default AlbumList;
+
+// props is for parent to child communication
+// state is for a components internal record keeping, use state to update data
